41e1a71631ae239eb0c2d6144dd0aa4dbda51112
Tauzu <87505664+Tauzu@users.noreply.github.com>
2025-08-16 22:06:17 +0900

    admin設定を追加

--- relay_manga/manga/admin.py
+++ relay_manga/manga/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Manga, Page
 
-# Register your models here.
+@admin.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_by', 'created_at')
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'manga', 'author', 'created_at', 'parent')
