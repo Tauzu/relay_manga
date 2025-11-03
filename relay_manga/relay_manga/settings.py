2c2634037f5438ed23fc0ec9d03ee6b86af9cf59
Tauzu <87505664+Tauzu@users.noreply.github.com>
2025-11-03 15:39:04 +0900

    デプロイ準備中

--- relay_manga/relay_manga/settings.py
+++ relay_manga/relay_manga/settings.py
@@ -26,6 +26,9 @@ LOGOUT_REDIRECT_URL = 'home'        # ログアウト後に行くURL
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False  # 本番では False
